1577d91ae8e25ad9e32a287159674af8c6815541
Raihan <raihanmohammad69@gmail.com>
2016-06-19 11:25:09 +0530

    Facebook Login Button added

---
+++ app/src/main/java/trainedge/projectq/App.java
@@ -0,0 +1,18 @@
+package trainedge.projectq;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by Raihan on 19/06/2016.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
