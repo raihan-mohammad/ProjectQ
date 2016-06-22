d8b4ff0035baace2d15737b3babcb376546d078c
Rashi Sachan <rashiaries.sachan@gmail.com>
2016-06-22 11:21:35 +0530

    22 June Rashi Sachan

--- app/src/main/java/trainedge/projectq/LoginPage.java
+++ app/src/main/java/trainedge/projectq/LoginPage.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
@@ -13,11 +15,12 @@ public class LoginPage extends AppCompatActivity {
     private TextView tv1;
     private TextView tv2;
     private TextView tv3;
-    private TextView et1;
-    private TextView et2;
-    private TextView b1;
-    private TextView b2;
-    private TextView b3;
+    private EditText et1;
+    private EditText et2;
+    private Button b1;
+    private Button b2;
+    private Button b3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +31,11 @@ public class LoginPage extends AppCompatActivity {
         tv1 =(TextView)findViewById(R.id.tv1);
         tv2 =(TextView)findViewById(R.id.tv2);
         tv3 =(TextView)findViewById(R.id.tv3);
-        et1 =(TextView)findViewById(R.id.et1);
-        et2 =(TextView)findViewById(R.id.et2);
-        b1 =(TextView)findViewById(R.id.b1);
-        b2 =(TextView)findViewById(R.id.b2);
-        b3 =(TextView)findViewById(R.id.b3);
+        et1 =(EditText)findViewById(R.id.et1);
+        et2 =(EditText)findViewById(R.id.et2);
+        b1 =(Button)findViewById(R.id.b1);
+        b2 =(Button)findViewById(R.id.b2);
+        b3 =(Button)findViewById(R.id.b3);
 
 
 
