16ae8781be3dc6632ed7a379751467aad2fe0e50
Rashi Sachan <rashiaries.sachan@gmail.com>
2016-06-28 11:25:19 +0530

    28 June Rashi Sachan

---
+++ app/src/main/java/trainedge/projectq/Play_three.java
@@ -0,0 +1,13 @@
+package trainedge.projectq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Play_three extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_three);
+    }
+}
